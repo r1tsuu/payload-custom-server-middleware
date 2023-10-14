b8f3e74118ab6b6b65c7553e0d524f321e52e390
Ritsu <enjoythepain1337@gmail.com>
2023-10-14 17:12:38 +0300

    custom server fixed middleware next

---
+++ examples/custom-server/src/middleware.ts
@@ -0,0 +1,21 @@
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest): void {
+  // eslint-disable-next-line no-console
+  console.log('HELLO FROM NEXT MIDDLEWARE', request.url)
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+}
